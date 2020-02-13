0ff1f5042197164239ee8aecda283be2c8eb6e71
Nadix92 <49558241+Nadix92@users.noreply.github.com>
2020-02-13 15:14:54 +0100

    node farm project v1 finish

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,27 +2,45 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const replaceTemplate = require("./modules/replaceTemplate");
+
 /////////////////////////
 // ##### Server ##### //
 ///////////////////////
 
 // read the file and get the data from it
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data); // Converts the json data to js object
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   // /, /overview
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+    res.end(output);
+
     // /product
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
     // /api
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
+
     // no page found error
   } else {
     res.writeHead(404, {
@@ -31,8 +49,6 @@ const server = http.createServer((req, res) => {
     });
     res.end("<h1>This path not found!</h1>");
   }
-
-  //   res.end("This is the responds from the server");
 });
 
 const PORT = process.env.PORT || 8000;
