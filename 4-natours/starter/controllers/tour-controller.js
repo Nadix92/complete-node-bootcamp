a9811512bf4ff9dc35f11114519b1ad1ed81e377
Nadix92 <49558241+Nadix92@users.noreply.github.com>
2020-02-16 16:53:38 +0100

    implemented CRUD operation for Tour

--- 4-natours/starter/controllers/tour-controller.js
+++ 4-natours/starter/controllers/tour-controller.js
@@ -1,56 +1,100 @@
 const Tour = require("./../models/tourModel");
 
-// Checks
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
+// Tours Handlers
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: "success",
+      results: tours.length,
+      data: {
+        tours
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
       status: "fail",
-      message: "Missing name or price"
+      message: err.message
     });
   }
-  next();
 };
 
-// Tours Handlers
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "success"
-    // results: tours.length,
-    // data: {
-    //   tours // = tours: tours
-    // }
-  });
-};
-exports.getTour = (req, res) => {
-  const id = +req.params.id;
-  // const tour = tours.find(el => el.id === id); // check if the id we type is === to the id in our document
+exports.getTour = async (req, res) => {
+  try {
+    // Tour.findOne({ _id: req.params.id }) =
+    const tour = await Tour.findById(req.params.id);
 
-  // res.status(200).json({
-  //   status: "success"
-  //   data: {
-  //     tour
-  //   }
-  // });
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message
+    });
+  }
 };
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: "success"
-    // data: {
-    //   tour: newTour
-    // }
-  });
+
+exports.createTour = async (req, res) => {
+  try {
+    // here we can make new tour from the post body the user send to the server
+    // Step 1: store newTour from req.body with await
+    const newTour = await Tour.create(req.body);
+
+    // Step 2: respond with status 201 and data from the newTour variable
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour
+      }
+    });
+    // Step 3: if something vent wrong we send this back to the user
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message
+    });
+  }
 };
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<Updated tour here...>"
-    }
-  });
+
+// This is a patch request (updates only filed we specify)
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message
+    });
+  }
 };
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: "success",
-    data: null
-  });
+
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: null
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message
+    });
+  }
 };
--- 4-natours/starter/unusedFiles/refrence.js
+++ 4-natours/starter/unusedFiles/refrence.js
@@ -50,6 +50,21 @@ const tours = JSON.parse(
 );
 
 /////////////////////////////////////////////
+// Checks
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Missing name or price"
+    });
+  }
+  next();
+};
+router
+  .route("/")
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
+
 exports.checkID = (req, res, next, val) => {
   console.log(`Tour id is: ${val}`);
   if (+req.params.id > tours.length) {
@@ -60,7 +75,6 @@ exports.checkID = (req, res, next, val) => {
   }
   next();
 };
-
 router.param("id", tourController.checkID);
 
 ////////////////////////////////////////////
