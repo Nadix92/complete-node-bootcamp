8a908595cf0c0a918e1f9a8c3966bc5fc697654c
Nadix92 <49558241+Nadix92@users.noreply.github.com>
2020-02-21 12:11:07 +0100

    update user data

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -1,5 +1,13 @@
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+
+const filterObj = (obj, ...allowedFields) => {
+  // TODO: CODE HERE
+  Object.keys(obj).forEach(el=> {
+    if(allowedFields.includes(el))
+  })
+}
 
 // User handlers
 exports.getAllUsers = catchAsync(async (req, res, next) => {
@@ -12,6 +20,24 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     data: {
       users
     }
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError("This route is not for password updates. Please use /updateMyPassword", 400));
+  }
+
+  // 2) Update user document
+  const filteredBody = filterObj(req.body, "name", "email");
+  const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: "success"
   });
 });
 
