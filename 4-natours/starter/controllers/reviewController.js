97d92e28c58573d68e68c106677628ca9b4f125c
Nadix92 <49558241+Nadix92@users.noreply.github.com>
2020-02-24 10:36:50 +0100

    delete by id and factory function added

--- 4-natours/starter/controllers/reviewController.js
+++ 4-natours/starter/controllers/reviewController.js
@@ -1,9 +1,13 @@
 const Review = require("./../models/reviewModel");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
+const factory = require("./handlerFactory");
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const review = await Review.find();
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const review = await Review.find(filter);
 
   // Send Responds
   res.status(200).json({
@@ -32,6 +36,10 @@ exports.getReview = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
   const newReview = await Review.create(req.body);
 
   res.status(201).json({
@@ -60,15 +68,17 @@ exports.updateReview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.deleteReview = catchAsync(async (req, res, next) => {
-  const review = await Review.findByIdAndDelete(req.params.id);
+exports.deleteReview = factory.deleteOne(Review);
 
-  if (!review) {
-    return next(new AppError("No review found with that ID", 404));
-  }
+// exports.deleteReview = catchAsync(async (req, res, next) => {
+//   const review = await Review.findByIdAndDelete(req.params.id);
 
-  res.status(204).json({
-    status: "success",
-    data: null
-  });
-});
+//   if (!review) {
+//     return next(new AppError("No review found with that ID", 404));
+//   }
+
+//   res.status(204).json({
+//     status: "success",
+//     data: null
+//   });
+// });
