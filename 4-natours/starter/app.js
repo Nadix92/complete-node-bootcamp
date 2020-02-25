9f373d932d98de06b7b0050ecd175d320bd32dd9
Nadix92 <49558241+Nadix92@users.noreply.github.com>
2020-02-25 15:06:58 +0100

    split footer and header to own folders

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,47 +1,47 @@
 // Third party modules
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const hpp = require("hpp");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 // Local modules
-const AppError = require("./utils/appError");
-const globalErrorHandler = require("./controllers/errorController");
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
-const reviewRouter = require("./routes/reviewRoutes");
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
-app.set("view engine", "pug"); // set pug as view engine
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'pug'); // set pug as view engine
+app.set('views', path.join(__dirname, 'views'));
 
 // Third party MiddleWares
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set Security HTTP Headers
 app.use(helmet());
 
 // Development logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 // Limits the api request from same user
 const limiter = rateLimit({
   max: 100, // x request
   windowMs: 60 * 60 * 1000, // 1hr
-  message: "Too many request from this IP, please try again in an hour!"
+  message: 'Too many request from this IP, please try again in an hour!'
 });
-app.use("/api", limiter);
+app.use('/api', limiter);
 
 // body parser, to be able to read json from a request e.g  req.body
-app.use(express.json({ limit: "10kb" })); // set max limit 10kb
+app.use(express.json({ limit: '10kb' })); // set max limit 10kb
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -52,24 +52,24 @@ app.use(xss());
 // Prevent parameter pollution
 app.use(
   hpp({
-    whitelist: ["duration", "ratingsAverage", "ratingsQuantity", "maxGroupSize", "difficulty", "price"]
+    whitelist: ['duration', 'ratingsAverage', 'ratingsQuantity', 'maxGroupSize', 'difficulty', 'price']
   })
 );
 
 // Routes
-app.get("/", (req, res) => {
-  res.status(200).render("base", {
-    tour: "The forest Hiker",
-    user: "Ole"
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The forest Hiker',
+    user: 'Ole'
   });
 });
 
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/reviews", reviewRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // Fail any routes that we don't specify above
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   // if a argument is passed to next() express know it was an error and all other middleware will be skipped
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const slugify = require("slugify");
+const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 // const User = require("./userModel");
 // const validator = require("validator");
@@ -9,35 +9,35 @@ const tourSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "A tour must have a name"], // second argument is the error
+      required: [true, 'A tour must have a name'], // second argument is the error
       unique: true, // Can only have one
       trim: true,
-      maxlength: [40, "A tour name must have less or equal then 40 characters"],
-      minlength: [10, "A tour name must have more or equal then 10 characters"]
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters']
       // validate: [validator.isAlpha, "A tour name must only contain characters"]
     },
     slug: String,
     duration: {
       type: Number,
-      required: [true, "A tour must have a duration"]
+      required: [true, 'A tour must have a duration']
     },
     maxGroupSize: {
       type: Number,
-      required: [true, "A tour must have a group size"]
+      required: [true, 'A tour must have a group size']
     },
     difficulty: {
       type: String,
-      required: [true, "A tour must have a difficulty"],
+      required: [true, 'A tour must have a difficulty'],
       enum: {
-        values: ["easy", "medium", "difficult"],
-        message: "Difficulty is either: easy, medium, difficult"
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult'
       }
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
-      min: [1, "Rating must be above 1.0 "],
-      max: [5, "Rating must be below 5.0"],
+      min: [1, 'Rating must be above 1.0 '],
+      max: [5, 'Rating must be below 5.0'],
       set: val => Math.round(val * 10) / 10
     },
     ratingsQuantity: {
@@ -46,7 +46,7 @@ const tourSchema = new mongoose.Schema(
     },
     price: {
       type: Number,
-      required: [true, "A tour must have a price"]
+      required: [true, 'A tour must have a price']
     },
     priceDiscount: {
       type: Number,
@@ -55,13 +55,13 @@ const tourSchema = new mongoose.Schema(
           // this only points to current doc on NEW document creation
           return val < this.price;
         },
-        message: "Discount price ({VALUE}) should be below the regular price"
+        message: 'Discount price ({VALUE}) should be below the regular price'
       }
     },
     summary: {
       type: String,
       trim: true,
-      required: [true, "A tour must have a description"]
+      required: [true, 'A tour must have a description']
     },
     description: {
       type: String,
@@ -69,7 +69,7 @@ const tourSchema = new mongoose.Schema(
     },
     imageCover: {
       type: String,
-      required: [true, "A tour must have a cover image"]
+      required: [true, 'A tour must have a cover image']
     },
     images: [String],
     createdAt: {
@@ -86,8 +86,8 @@ const tourSchema = new mongoose.Schema(
       // GeoJSON
       type: {
         type: String,
-        default: "Point",
-        enum: ["Point"]
+        default: 'Point',
+        enum: ['Point']
       },
       coordinates: [Number],
       address: String,
@@ -97,8 +97,8 @@ const tourSchema = new mongoose.Schema(
       {
         type: {
           type: String,
-          default: "Point",
-          enum: ["Point"]
+          default: 'Point',
+          enum: ['Point']
         },
         coordinates: [Number],
         address: String,
@@ -106,7 +106,7 @@ const tourSchema = new mongoose.Schema(
         day: Number
       }
     ],
-    guides: [{ type: mongoose.Schema.ObjectId, ref: "User" }] // Get id from User document and store in an array for ref
+    guides: [{ type: mongoose.Schema.ObjectId, ref: 'User' }] // Get id from User document and store in an array for ref
   },
 
   {
@@ -118,21 +118,21 @@ const tourSchema = new mongoose.Schema(
 // tourSchema.index({ price: 1 });
 tourSchema.index({ price: 1, ratingsAverage: -1 });
 tourSchema.index({ slug: 1 });
-tourSchema.index({ startLocation: "2dsphere" });
+tourSchema.index({ startLocation: '2dsphere' });
 
-tourSchema.virtual("durationWeeks").get(function() {
+tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
 // Virtual populate
-tourSchema.virtual("reviews", {
-  ref: "Review",
-  foreignField: "tour", // field in reviewSchema where the tour id is being stored
-  localField: "_id"
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour', // field in reviewSchema where the tour id is being stored
+  localField: '_id'
 });
 
 // Document Middleware: runs  only before .save() and .create() but not on .insertMany() or any other
-tourSchema.pre("save", function(next) {
+tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
@@ -165,8 +165,8 @@ tourSchema.pre(/^find/, function(next) {
 
 tourSchema.pre(/^find/, function(next) {
   this.populate({
-    path: "guides",
-    select: "-__v -passwordChangedAt"
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
   });
 
   next();
@@ -185,6 +185,6 @@ tourSchema.post(/^find/, function(docs, next) {
   next();
 }); */
 
-const Tour = mongoose.model("Tour", tourSchema);
+const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
