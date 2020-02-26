85f588d3480bb0a0f5f6aab4f7b78a0d78b4698a
Nadix92 <49558241+Nadix92@users.noreply.github.com>
2020-02-26 15:17:52 +0100

    Update import-dev-data.js

--- 4-natours/starter/dev-data/data/import-dev-data.js
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -1,14 +1,14 @@
-const fs = require("fs");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const Tour = require("./../../models/tourModel");
-const User = require("./../../models/userModel");
-const Review = require("./../../models/reviewModel");
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const Tour = require('./../../models/tourModel');
+const User = require('./../../models/userModel');
+const Review = require('./../../models/reviewModel');
 
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: './.env' });
 
 // Connect to the DB
-const DB = process.env.DB_STRING.replace("<PASSWORD>", process.env.DB_PW);
+const DB = process.env.DB_STRING.replace('<PASSWORD>', process.env.DB_PW);
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -16,12 +16,12 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true
   })
-  .then(() => console.log("DB connection successful!"));
+  .then(() => console.log('DB connection successful!'));
 
 // READ JSON FILE
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8")); // Put path to ur Json Data File here
-const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8")); // Put path to ur Json Data File here
-const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, "utf-8")); // Put path to ur Json Data File here
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')); // Put path to ur Json Data File here
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8')); // Put path to ur Json Data File here
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')); // Put path to ur Json Data File here
 
 // Import data in to database
 const importData = async () => {
@@ -29,7 +29,7 @@ const importData = async () => {
     await Tour.create(tours);
     await User.create(users, { validateBeforeSave: false });
     await Review.create(reviews);
-    console.log("Data successfully loaded!");
+    console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
   }
@@ -41,7 +41,7 @@ const deleteData = async () => {
     await Tour.deleteMany();
     await User.deleteMany();
     await Review.deleteMany();
-    console.log("Data successfully deleted from collections");
+    console.log('Data successfully deleted from collections');
   } catch (err) {
     console.log(err);
   }
